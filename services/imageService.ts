69705fcf5bbf8c166b85a6bcd2357f49ddb57806
GunNapat <napat.wat@innovasive.co.th>
2022-04-20 15:54:31 +0700

    🌸 add: morgan logger

--- services/imageService.ts
+++ services/imageService.ts
@@ -1,12 +1,13 @@
-import { errorText } from "@coreTypes/core";
 import createError from "http-errors";
 import _ from "lodash";
 import multer from "multer";
 import path from "path";
 
+import { errorText } from "@coreTypes/core";
+
 const storage = multer.diskStorage({
   destination: function (req, _file, cb) {
-    cb(null, `public/${req.baseUrl.substring(1)}`);
+    cb(null, `public/images/${req.baseUrl.substring(1)}`);
   },
   filename: function (_req, file, cb) {
     cb(null, file.originalname);
