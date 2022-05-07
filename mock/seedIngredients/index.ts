1f7db2bfad8d6298e4914ee327bfe9421ecb4340
GunNapat <napat.wat@innovasive.co.th>
2022-05-07 17:31:18 +0700

    🌸 add: seed ingredient details w/o images

--- mock/seedIngredients/index.ts
+++ mock/seedIngredients/index.ts
@@ -1,5 +1,21 @@
-import seedSeasonings from './seedSeasonings';
+import seedDairy from '@mock/seedIngredients/seedDairy';
+import seedSeasonings from '@mock/seedIngredients/seedSeasonings';
+import seedSeafood from '@mock/seedIngredients/seedSeafood';
+import seedLegumes from '@mock/seedIngredients/seedLegumes';
+import seedMeat from '@mock/seedIngredients/seedMeat';
+import seedFlour from '@mock/seedIngredients/seedFlour';
+import seedManufactured from '@mock/seedIngredients/seedManufactured';
+import seedVegetables from '@mock/seedIngredients/seedVegetables';
 
-const seedIngredients = [...seedSeasonings];
+const seedIngredients = [
+  ...seedSeasonings,
+  ...seedDairy,
+  ...seedSeafood,
+  ...seedLegumes,
+  ...seedMeat,
+  ...seedFlour,
+  ...seedManufactured,
+  ...seedVegetables,
+];
 
 export default seedIngredients;
