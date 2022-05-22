71b3214251aaf49e8d538cd3e09caedffa70d583
GunNapat <napat.wat@innovasive.co.th>
2022-05-22 18:15:08 +0700

    🌸 add: constraint config

---
+++ config/constraint.ts
@@ -0,0 +1,8 @@
+export default {
+  username: { min: 6, max: 30 },
+  password: { min: 8, max: 32 },
+  desc: { max: 500 },
+  caption: { max: 500 },
+  comment: { max: 500 },
+  detail: { max: 200 },
+};
