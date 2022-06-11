4280982f5dc809e2a4044e26a46e0a2aa0f0216b
GunNapat <napat.wat@innovasive.co.th>
2022-06-11 15:19:15 +0700

    🌸 update: merge cooking methods

--- mock/seedCookingMethods.ts
+++ mock/seedCookingMethods.ts
@@ -1,7 +1,6 @@
 import { Types } from 'mongoose';
 
 export default [
-  { _id: new Types.ObjectId('626c3a0320e1318208666f7f'), name: 'ย่าง' },
   { _id: new Types.ObjectId('626c3a060254ba5cd96b50c2'), name: 'อบ' },
   { _id: new Types.ObjectId('626c3a0a6b6fe9a8ca46dda8'), name: 'ต้ม' },
   { _id: new Types.ObjectId('626c3a0f172511708027e8eb'), name: 'ทอด' },
@@ -10,8 +9,7 @@ export default [
   { _id: new Types.ObjectId('626c3a1beafd981b648d50c0'), name: 'ตุ๋น' },
   { _id: new Types.ObjectId('626c3a1e54e487dfb0234d3b'), name: 'ลวก' },
   { _id: new Types.ObjectId('626c3a23be5104f99fef68d7'), name: 'ผัด' },
-  { _id: new Types.ObjectId('626c3a279c65b3a236797b53'), name: 'ปิ้ง' },
-  { _id: new Types.ObjectId('626c3a2a60c7637219acc40b'), name: 'ยำ' },
-  { _id: new Types.ObjectId('626c3a2e0ecae1a40bef72f6'), name: 'ดอง' },
+  { _id: new Types.ObjectId('626c3a279c65b3a236797b53'), name: 'ปิ้งย่าง' },
+  { _id: new Types.ObjectId('626c3a2a60c7637219acc40b'), name: 'ยำดอง' },
   { _id: new Types.ObjectId('626c3a31d1e8e4d36604d408'), name: 'แช่เย็น' },
 ];
