2ba451522008f05872c9101666d70aad7053b512
GunNapat <napat.wat@innovasive.co.th>
2022-07-18 16:19:52 +0700

    🌸 add: add and revoke admin template

--- services/mailService.ts
+++ services/mailService.ts
@@ -3,6 +3,7 @@ import hbs from 'nodemailer-express-handlebars';
 
 const user = process.env.MAIL_USER;
 const pass = process.env.MAIL_PASS;
+const url = process.env.FE_URL;
 
 const transport = nodemailer.createTransport({
   service: 'Gmail',
@@ -30,8 +31,8 @@ export const sendAdminConfirmation = async (email: string, uniqueKey: string) =>
     from: user,
     to: email,
     subject: 'คุณได้รับคำเชิญให้เป็นผู้ดูแลระบบของ Cookify Web Application',
-    template: 'test',
-    context: { uniqueKey },
+    template: 'admin_invitation',
+    context: { link: `${url}/register?uniqueKey=${uniqueKey}` },
   };
   await transport.sendMail(options);
 };
@@ -41,7 +42,8 @@ export const sendAdminRevocation = async (email: string) => {
     from: user,
     to: email,
     subject: 'คุณได้ถูกถอดสิทธิ์ในการเป็นผู้ดูแลระบบของ Cookify Web Application',
-    template: 'test',
+    template: 'revoke_admin',
+    context: { link: `${url}/register` },
   };
   await transport.sendMail(options);
 };
