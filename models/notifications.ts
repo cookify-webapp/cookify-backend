93c0ed00c2e290bf6eb6d50be31117333fdc1ca0
GunNapat <napat.wat@innovasive.co.th>
2022-08-09 12:08:07 +0700

    🌸 add: complaint + noti model

---
+++ models/notifications.ts
@@ -0,0 +1,56 @@
+import { model, Schema, Model, Document, Types } from 'mongoose';
+import { AccountInstanceInterface } from './account';
+
+//---------------------
+//   INTERFACE
+//---------------------
+export interface NotificationInterface extends Document {
+  _id: Types.ObjectId;
+  type: 'complaint' | 'follow' | 'comment';
+  caption: string;
+  link: string;
+  receiver: Types.ObjectId & AccountInstanceInterface;
+}
+
+export interface NotificationInstanceMethods {
+  // declare any instance methods here
+}
+
+export interface NotificationInstanceInterface extends NotificationInterface, NotificationInstanceMethods {}
+
+export interface NotificationModelInterface extends Model<NotificationInstanceInterface, NotificationQueryHelpers> {
+  // declare any static methods here
+}
+
+interface NotificationQueryHelpers {}
+
+//---------------------
+//   SCHEMA
+//---------------------
+export const notificationSchema = new Schema<
+  NotificationInstanceInterface,
+  NotificationModelInterface,
+  NotificationInstanceMethods,
+  NotificationQueryHelpers
+>(
+  {
+    type: { type: String, enum: ['complaint', 'follow', 'comment'], required: true },
+    caption: { type: String, required: true },
+    link: { type: String, required: true },
+    receiver: { type: 'ObjectId', ref: 'Account', required: true },
+  },
+  {
+    autoCreate: process.env.NODE_ENV !== 'production',
+    collation: { locale: 'th' },
+    timestamps: { createdAt: true, updatedAt: false },
+    versionKey: false,
+  }
+);
+
+//---------------------
+//   MODEL
+//---------------------
+export const Notification = model<NotificationInstanceInterface, NotificationModelInterface>(
+  'Notification',
+  notificationSchema
+);
